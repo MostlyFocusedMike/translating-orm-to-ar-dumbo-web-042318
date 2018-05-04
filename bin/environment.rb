63041819b7d79aa621b7977e3852e2a848e83849
Mike C <mikecronin92@gmail.com>
2018-05-04 20:21:47 +0000

    automatically backed up by learn

--- bin/environment.rb
+++ bin/environment.rb
@@ -6,12 +6,12 @@ require 'active_record'
 require_relative '../lib/dog'
 require_relative 'sql_runner'
 
-DB = ApplicationRecord.establish_connection(
+DB = ActiveRecord::Base.establish_connection(
       :adapter => "sqlite3",
       :database => "../db/dogs"
     )
 
-  DB = ApplicationRecord.connection
+  DB = ActiveRecord::Base.connection
 
   if ENV["ACTIVE_RECORD_ENV"] == "test"
     ActiveRecord::Migration.verbose = false
